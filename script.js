4647ea083476bc4d9b431926d7faeae8c3b13223
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-11-30 13:39:29 +0000

    Create and Code getHumanChoice()

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 "use strict";
 
-function getComputerChoice () {
+function getComputerChoice() {
     let randomNumber;
     let computerChoice;
     randomNumber = Math.round(Math.random() * (3 - 1) + 1);
     switch(randomNumber) {
         case 1:
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
         case 2:
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
         case 3:
-            computerChoice = "Scissors"
+            computerChoice = "scissors"
             break;
     }
     return computerChoice;
 }
+
+function getHumanChoice() {
+    let humanChoice;
+    humanChoice = prompt("Please choose Rock, Paper or Scissors");
+    humanChoice = humanChoice.trim();
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+        return humanChoice;
+    }
+    else {
+        alert(`Invalid option.\nPlease choose again.`);
+    }
+}
+
 
 
 
