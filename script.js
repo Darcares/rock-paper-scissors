7f8ce62f17989d511249c56f81af119576ab285d
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-11-30 13:00:01 +0000

    Create and Code getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("linked =D");
+"use strict";
+
+function getComputerChoice () {
+    let random;
+    let choice;
+    random = Math.round(Math.random() * (3 -1) + 1);
+    switch(random) {
+        case 1:
+            choice = "Rock";
+            break;
+        case 2:
+            choice = "Paper";
+            break;
+        case 3:
+            choice = "Scissors"
+            break;
+    }
+    return choice;
+}
+
+
+
+
