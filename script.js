445a62cc2cdd81420fd04190b54bb26dbaeae958
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-11-30 16:22:48 +0000

    Code checkTie()

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function getHumanChoice() {
     let humanChoice;
     let keepGoing = true;
     while(keepGoing) {
-        humanChoice = prompt("Please type Rock, Paper or Scissors");
+        //humanChoice = prompt("Please type Rock, Paper or Scissors");
         humanChoice = humanChoice.trim();
         humanChoice = humanChoice.toLowerCase();
         if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
@@ -41,7 +41,6 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     let choicesComparison;
     let isHumanWinner;
-    let itsTie;
     choicesComparison = `${humanChoice}-${computerChoice}`;
     switch(choicesComparison) {
         case "paper-rock":
@@ -49,7 +48,6 @@ function playRound(humanChoice, computerChoice) {
         case "scissors-paper":
             console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nHuman wins this round.`);
             isHumanWinner = true;
-            itsTie = false;
             break;
 
         case "rock-paper":
@@ -57,7 +55,6 @@ function playRound(humanChoice, computerChoice) {
         case "scissors-rock":
             console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nComputer wins this round.`);
             isHumanWinner = false;
-            itsTie = false;
             break;
 
         case "rock-rock":
@@ -65,6 +62,35 @@ function playRound(humanChoice, computerChoice) {
         case "scissors-scissors":
             console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nIt's a tie this round.`);
             isHumanWinner = false;
+            break;
+
+        default:
+            console.log("Investigate error");
+            break;
+    }
+    return isHumanWinner;   
+}
+
+function checkTie(humanChoice, computerChoice) {
+    let choicesComparison;
+    let itsTie;
+    choicesComparison = `${humanChoice}-${computerChoice}`;
+    switch(choicesComparison) {
+        case "paper-rock":
+        case "rock-scissors":
+        case "scissors-paper":
+            itsTie = false;
+            break;
+
+        case "rock-paper":
+        case "paper-scissors":
+        case "scissors-rock":
+            itsTie = false;
+            break;
+
+        case "rock-rock":
+        case "paper-paper":
+        case "scissors-scissors":
             itsTie = true;
             break;
 
@@ -72,33 +98,41 @@ function playRound(humanChoice, computerChoice) {
             console.log("Investigate error");
             break;
     }
-    return isHumanWinner, itsTie;
+    return itsTie;   
 }
 
 function calculateScore(isHumanWinner, itsTie, humanScore, computerScore) {
     if(itsTie) {
- 
+        console.log(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
     }
+
     else {
-        if(isHumanWinner){
+        if(isHumanWinner) {
             humanScore++;
+            console.log(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
+            return humanScore;
         }
     
         else {
             computerScore++;
+            console.log(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
+            return computerScore;
         } 
     }
-    console.log(`Human score: ${humanScore}\nComputer score: ${computerScore}`)
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    let isHumanWinner = null;
-    let itsTie = null;
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    isHumanWinner, itsTie = playRound(humanChoice, computerChoice);
-    calculateScore(isHumanWinner, itsTie, humanScore, computerScore);
+    let isHumanWinner;
+    let itsTie;
+    alert(`You will play best of 5`)
+    for(let round = 1; round <=5; round++) {
+        console.log(`Round #${round}`)
+        const humanChoice = "rock";
+        const computerChoice = "scissors";
+        isHumanWinner = playRound(humanChoice, computerChoice);
+        itsTie = checkTie(humanChoice, computerChoice);
+    }
 }
 
