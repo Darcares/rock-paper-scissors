e976ff585ef2e9ceceaeefc5547c862d4572c66e
Daniel Cárdenas <58755748+Darcares@users.noreply.github.com>
2024-11-30 14:30:34 +0000

    Create and Code playRound()

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
             computerChoice = "paper";
             break;
         case 3:
-            computerChoice = "scissors"
+            computerChoice = "scissors";
             break;
     }
     return computerChoice;
@@ -20,18 +20,50 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice;
-    humanChoice = prompt("Please choose Rock, Paper or Scissors");
-    humanChoice = humanChoice.trim();
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        return humanChoice;
-    }
-    else {
-        alert(`Invalid option.\nPlease choose again.`);
+    let keepGoing = true;
+    while(keepGoing) {
+        humanChoice = prompt("Please type Rock, Paper or Scissors");
+        humanChoice = humanChoice.trim();
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            keepGoing = false;
+            return humanChoice;
+        }
+        else {
+            keepGoing = true;
+            alert(`Invalid option.\nPlease choose again.`);
+        }
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    let comparison;
+    comparison = `${humanChoice}-${computerChoice}`;
+    switch(comparison) {
+        case "paper-rock":
+        case "rock-scissors":
+        case "scissors-paper":
+            console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nHuman wins.`);
+            break;
 
+        case "rock-paper":
+        case "paper-scissors":
+        case "scissors-rock":
+            console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nComputer wins.`);
+            break;
 
+        case "rock-rock":
+        case "paper-paper":
+        case "scissors-scissors":
+            console.log(`Human: ${humanChoice}.\nComputer: ${computerChoice}.\nIt's a tie.`);
+            break;
 
+        default:
+            console.log("Investigate error");
+            break;
+    }
+}
 
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
